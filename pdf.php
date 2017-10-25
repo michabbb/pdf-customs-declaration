76852d112e13751962c5f087a410f49af65b6c27
micha <info@macropage.de>
2017-10-25 21:35:49 +0200

    add html table

--- pdf.php
+++ pdf.php
@@ -25,5 +25,54 @@ $pdf->SetSubject('customs declaration');
 //set image scale factor
 //$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 //$pdf->startPageGroup();
-//$pdf->AddPage();
+$pdf->AddPage();
+
+$tbl = <<<EOD
+<table border="1" cellpadding="10" nobr="true">
+<tbody>
+<tr>
+<td width="210"><b><font size="15">CUSTOMS<br />DECLRATION</font></b></td>
+<td width="100">&nbsp;</td>
+<td width="100">&nbsp;</td>
+<td width="120"><b><font size="30">CN22</font></b></td>
+</tr>
+<tr>
+<td width="210"><img src="images/checkbox_checked.png"><br>Gift</td>
+<td width="100"><img src="images/checkbox_unchecked.png"><br>Documents</td>
+<td width="100"><img src="images/checkbox_unchecked.png"><br>Commercial<br>sample</td>
+<td width="120"><img src="images/checkbox_unchecked.png"><br>Other</td>
+</tr>
+<tr>
+	<th colspan="2">Quantity and detailed description of contents</th>
+<td width="100">Weight (kg)</td>
+<td width="100">Value</td>
+</tr>
+<tr>
+<th colspan="2"><br><i>2 x costumes</i></th>
+<td width="100"><i>0,200</i></td>
+<td width="120"><i>3,50 EUR</i></td>
+</tr>
+<tr>
+<td width="210">&nbsp;</td>
+<td width="100">&nbsp;</td>
+<td width="100">Total Weight (kg)</td>
+<td width="120">Total Value</td>
+</tr>
+<tr>
+<td width="210">&nbsp;</td>
+<td width="100">&nbsp;</td>
+<td width="100"><b><i>0,200</i></b></td>
+<td width="120"><b><i>3,50 EUR</i></b></td>
+</tr>
+<tr>
+<td colspan="4">I, the undersigned whose name and address are given on the item, certify that the particulars given in
+this Declaration are correct and that this item does not contain any dangerous article or articles
+prohibited by legislation or by postal or customs regulations.<br><br><br><br><b>Date and sender’s signature</b></td>
+</tr>
+</tbody>
+</table>
+EOD;
+
+$pdf->writeHTML($tbl);
+
 $pdf->Output('/tmp/customs_declaration.pdf', 'F');
