e792fd8cbea72ea90906d26b9e25ab50cf40aaa1
micha <info@macropage.de>
2017-11-01 15:04:12 +0100

    adding france + signature

--- pdf.php
+++ pdf.php
@@ -31,21 +31,25 @@ $tbl = <<<EOD
 <table border="1" cellpadding="10" nobr="true">
 <tbody>
 <tr>
-<td width="210"><b><font size="15">CUSTOMS<br />DECLRATION</font></b></td>
+<td width="210"><b><font size="15">CUSTOMS<br />DECLRATION</font><br><font size="11">Déclaration en douane</font></b></td>
 <td width="100">&nbsp;</td>
 <td width="100">&nbsp;</td>
 <td width="120"><b><font size="30">CN22</font></b></td>
 </tr>
 <tr>
-<td width="210"><img src="images/checkbox_checked.png"><br>Gift</td>
-<td width="100"><img src="images/checkbox_unchecked.png"><br>Documents</td>
-<td width="100"><img src="images/checkbox_unchecked.png"><br>Commercial<br>sample</td>
-<td width="120"><img src="images/checkbox_unchecked.png"><br>Other</td>
+<td width="210"><b><font size="15">COUNTRY / CITY</font></b><br><font size="11">Pays de destination</font></td>
+<th colspan="3" >&nbsp;</th>
 </tr>
 <tr>
-	<th colspan="2">Quantity and detailed description of contents</th>
-<td width="100">Weight (kg)</td>
-<td width="100">Value</td>
+<td width="210"><img src="images/checkbox_checked.png"><br>Merchandise<br>Marchandise</td>
+<td width="100"><img src="images/checkbox_unchecked.png"><br>Documents</td>
+<td width="100"><img src="images/checkbox_unchecked.png"><br>Commercial<br>sample<br>échantillon commercial</td>
+<td width="120"><img src="images/checkbox_unchecked.png"><br>Gift<br>Cadeau</td>
+</tr>
+<tr>
+	<th colspan="2">Quantity and detailed description of contents<br>Quantité et description détaillée du contenu</th>
+<td width="100">Weight (kg)<br>valeur</td>
+<td width="100">Value<br>valeur</td>
 </tr>
 <tr>
 <th colspan="2"><br><i>2 x costumes</i></th>
@@ -55,8 +59,8 @@ $tbl = <<<EOD
 <tr>
 <td width="210">&nbsp;</td>
 <td width="100">&nbsp;</td>
-<td width="100">Total Weight (kg)</td>
-<td width="120">Total Value</td>
+<td width="100">Total Weight (kg)<br>Poids total</td>
+<td width="120">Total Value<br>Valeur totale</td>
 </tr>
 <tr>
 <td width="210">&nbsp;</td>
@@ -67,7 +71,15 @@ $tbl = <<<EOD
 <tr>
 <td colspan="4">I, the undersigned whose name and address are given on the item, certify that the particulars given in
 this Declaration are correct and that this item does not contain any dangerous article or articles
-prohibited by legislation or by postal or customs regulations.<br><br><br><br><b>Date and sender’s signature</b></td>
+prohibited by legislation or by postal or customs regulations.<br><br>
+
+Je, soussigné (e), dont le nom et l'adresse figurent sur l'envoi, certifie que les indications données dans
+cette déclaration est correcte et que cet article ne contient aucun article ou article dangereux
+interdite par la législation ou par la réglementation postale ou douanière.
+
+<br><br><b>Date and sender’s signature</b><br><br>
+<b>01.11.2017</b>&nbsp;<img src="images/signature.jpg">
+</td>
 </tr>
 </tbody>
 </table>
